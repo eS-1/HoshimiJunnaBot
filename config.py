b0a39941eb725e9179945e3333fc56d5a59e9619
eS-1 <cybergun00@gmail.com>
2023-04-16 23:04:22 +0900

    各種tokenの再記述

--- config.py
+++ config.py
@@ -2,4 +2,9 @@ import os
 import tweepy
 
 
-myClient = tweepy.Client(bearer_token=os.environ["BEARER_TOKEN"])
+myClient = tweepy.Client(
+    bearer_token=os.environ["BEARER_TOKEN"],
+    consumer_key=os.environ["API_KEY"],
+    consumer_secret=os.environ["API_KEY_SECRET"],
+    access_token=os.environ["ACCESS_TOKEN"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"])
