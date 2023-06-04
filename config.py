a194a64abb05be92a2655893df18def372646a4a
eS-1 <cybergun00@gmail.com>
2023-06-04 16:36:21 +0900

    redeployのためのcommit

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ myClient = tweepy.Client(
     consumer_key=os.environ["API_KEY"],
     consumer_secret=os.environ["API_KEY_SECRET"],
     access_token=os.environ["ACCESS_TOKEN"],
-    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"])
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+)
