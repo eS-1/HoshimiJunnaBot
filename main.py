d70e5f3a5e808fe079be42887576cec80c7a11be
eS-1 <cybergun00@gmail.com>
2023-06-20 22:37:27 +0900

    start_dateの変更

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tweetText import tweetText
 twische = BlockingScheduler()
 
 
-@twische.scheduled_job('interval', minutes=30, start_date="2023-06-04 17:30:00")
+@twische.scheduled_job('interval', minutes=30, start_date="2023-06-20 23:00:00")
 def timed_job():
     tweetText()
 
